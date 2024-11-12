8cceb575bf9c75955c36c35f3f663b59c9b9add1
meheraz1100 <meheraz1100@gmail.com>
2024-11-12 09:53:55 +0600

    typescript class and object

--- src/class_&_object.ts
+++ src/class_&_object.ts
@@ -2,15 +2,10 @@
 
 // oop-- class
 class Animal {
-    name: string;
-    species: string;
-    sound: string;
 
-    constructor(name: string, species: string, sound: string){
-        this.name = name;
-        this.species = species;
-        this.sound = sound;
-    }
+    // parameter properties 
+
+    constructor(public name: string,public species: string,public  sound: string){}
 
 
     makeSound(){
@@ -25,7 +20,7 @@ const cat = new Animal("Persian", "cat", "Meow Meow");
 
 
 cat.makeSound();
-
+dog.makeSound();
 
 
 
