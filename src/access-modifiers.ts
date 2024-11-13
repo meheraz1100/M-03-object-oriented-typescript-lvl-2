91d6b2660ec532ec3aeb75b3688b4487819db74d
meheraz1100 <meheraz1100@gmail.com>
2024-11-13 07:27:44 +0600

    access modifiers

---
+++ src/access-modifiers.ts
@@ -0,0 +1,49 @@
+{
+
+// accress modifiers
+
+class BankAccount {
+    public readonly id: number;
+    name: string;
+    protected _balance: number;
+
+
+    constructor(id: number, name: string, balance: number){
+        this.id = id;
+        this.name = name;
+        this._balance = balance;
+    }
+    public addDeposit(amount : number){
+        this._balance = this._balance + amount;
+    }
+
+    public getBalance(){
+        return this._balance;
+    }
+}
+
+class StudentAccount extends BankAccount{
+    test(){
+        // this.
+    }
+}
+
+const goribManusherAccount = new BankAccount(111, "mr. gorib", 20);
+
+// goribManusherAccount.balance = 0
+goribManusherAccount.addDeposit(2980)
+const myBalance = goribManusherAccount.getBalance()
+console.log(myBalance);
+
+
+
+
+
+
+
+
+
+
+
+
+}
